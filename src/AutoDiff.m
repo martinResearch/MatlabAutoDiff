009c297b90a8c8844a1fc595979ac21359eb1f14
AliG <info@gha3mi.com>
2022-05-13 20:26:10 +0200

    added acosh with tests. refs#14

--- src/AutoDiff.m
+++ src/AutoDiff.m
@@ -112,6 +112,11 @@ classdef AutoDiff
             x.values = abs(x.values);
         end
 
+        function x = acosh(x)
+            x.derivatives = AutoDiff.spdiag(1./sqrt((x.values).^2-1)) * x.derivatives;
+            x.values = acosh(x.values);
+        end
+
         function x = sqrt(x)
             x.values = sqrt(x.values);
             x.derivatives = AutoDiff.spdiag(0.5./x.values) * x.derivatives;
